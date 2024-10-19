30931f0f02847b214015d9a0146759c85e50aecb
LAHEE030 <82334926+LAHEE030@users.noreply.github.com>
2024-10-19 12:36:08 +0800

    Update publishFeedGen.ts

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -8,28 +8,28 @@ const run = async () => {
 
   // YOUR bluesky handle
   // Ex: user.bsky.social
-  const handle = ''
+  const handle = 'lacu-bluejay.bsky.social'
 
   // YOUR bluesky password, or preferably an App Password (found in your client settings)
   // Ex: abcd-1234-efgh-5678
-  const password = ''
+  const password = 'LHBY3344520'
 
   // A short name for the record that will show in urls
   // Lowercase with no spaces.
   // Ex: whats-hot
-  const recordName = ''
+  const recordName = '舟德莱'
 
   // A display name for your feed
   // Ex: What's Hot
-  const displayName = ''
+  const displayName = '舟德莱'
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
-  const description = ''
+  const description = '明日方舟-德米特里x莱昂图索'
 
   // (Optional) The path to an image to be used as your feed's avatar
   // Ex: ~/path/to/avatar.jpeg
-  const avatar: string = ''
+  const avatar: string = 'https://ibb.co/5WGbX4X/path/to/avatar.jpeg'
 
   // -------------------------------------
   // NO NEED TO TOUCH ANYTHING BELOW HERE
